f19cc5febf48bb10be2baf43706fe31cd14bc1c2
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-04 22:21:01 +0800

    adding comments to app.js

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const userRoutes = require('./routes/user');
-
 const app = express();
 
+// Middleware
 app.use(bodyParser.json());
 
+// Routes
 app.use('/user', userRoutes);
 
+// Starting the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
