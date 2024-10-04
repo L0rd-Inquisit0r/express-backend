8fb980380d56626decfd8c45437c3f25700016b4
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-04 22:46:42 +0800

    updated model file to read and write from users.json

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,18 @@
-let users = [
-    { id: 1, username: 'john_doe', password: '123456', email: 'john@example.com' }
-  ];
-  
-  module.exports = { users };
-  
+const fs = require('fs');
+const path = require('path');
+
+// Path to the users.json file
+const usersFilePath = path.join(__dirname, '../data/users.json');
+
+// Read users from the file
+const getUsers = () => {
+  const usersData = fs.readFileSync(usersFilePath, 'utf-8');
+  return JSON.parse(usersData);
+};
+
+// Write users to the file
+const saveUsers = (users) => {
+  fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+};
+
+module.exports = { getUsers, saveUsers };
