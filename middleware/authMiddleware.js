f211dcf531dca3c15837f84963c2acc4e1a16e82
L0rd-Inquisit0r <sean.s.l.duran@gmail.com>
2024-10-05 00:37:21 +0800

    updated middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) {
+  const authHeader = req.headers['authorization']; // Check for 'authorization'
+  if (!authHeader) {
     return res.status(403).json({ message: 'No token provided' });
   }
 
+  const token = authHeader.split(' ')[1]; // Split to get the token after 'Bearer'
+  
   jwt.verify(token, 'your_secret_key', (err, decoded) => {
     if (err) {
       return res.status(500).json({ message: 'Failed to authenticate token' });
     }
 
-    req.userId = decoded.id;
-    next();
+    req.userId = decoded.id; // Save decoded id to req object
+    next(); // Pass to the next handler
   });
 };
 
